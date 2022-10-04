2fa920bc424f4533ca727baa5d504abd526c49a6
VasckMe <kasarin.anton@gmail.com>
2022-10-04 21:02:47 +0300

    Project created

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Apple Macbook Pro 13 on 4.10.22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
